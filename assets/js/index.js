c03bfe2ddb51fe056ecdf3e3cb0f35ddca148ea9
iGeek <jesus.cardenas.d@gmail.com>
2023-03-16 22:13:42 -0500

    corte clase jueves

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,72 @@
     const BASE_URL = "https://animales-api.onrender.com";
     const myChart = document.getElementById('myChart').getContext('2d');
     const tblAnimales = document.getElementById('tblAnimales');
+    // Esto son inputs de texto/number
+    const txtNombre = document.getElementById("txtNombre");
+    const txtCantidad = document.getElementById("txtCantidad");
+    const txtId = document.getElementById("txtId");
+    // Esto son los botones
+    const btnAgregar = document.getElementById("btnAgregar");
+    const btnEditar = document.getElementById("btnEditar");
+    const btnEliminar = document.getElementById("btnEliminar");
+
+
+    btnAgregar.addEventListener("click", ()=> {
+        console.log(txtNombre.value);
+        console.log(txtCantidad.value);
+
+        let nombre = txtNombre.value;
+        let cantidad = txtCantidad.value;
+        fetch(BASE_URL + "/animales",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                nombre: nombre,
+                cantidad: cantidad
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            // refresh de los datos
+            document.location.reload();
+        })
+        .catch(error => console.log(error))
+
+        console.log("Entro a agregar");
+    });
+
+    btnEditar.addEventListener("click", ()=> {
+        console.log(txtId.value);
+        console.log(txtNombre.value);
+        console.log(txtCantidad.value);
+        let id = txtId.value;
+        let objectRequest = {
+            nombre: txtNombre.value,
+            cantidad: txtCantidad.value
+        };
+        fetch(BASE_URL + "/animales/" + id,
+        {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(objectRequest)
+        })
+        .then(response => response.json())
+        .then(response => {
+            // refresh de los datos
+            document.location.reload();
+        })
+        .catch(error => console.log(error))
+        console.log("Entro a editar");
+    });
+
+    btnEliminar.addEventListener("click", ()=> {
+        console.log("Entro a elimnar");
+    });
 
 
 
@@ -19,7 +85,7 @@
                 console.log(response.data)
                 // Esto se realizo para terner el arreglo de string
                 let labels_for_chart = response.data.map((item) => {
-                    return item.nombre;
+                    return item.nombre.toUpperCase();
                 });
                 // Esto se realizo para obtener el arreglo de numeros
                 let data_for_chart = response.data.map((item) => {
@@ -52,7 +118,7 @@
                     // <tr>.....</tr>
                     let tr = `<tr>
                         <td>${animal.id}</td>
-                        <td>${animal.nombre}</td>
+                        <td>${animal.nombre.toUpperCase()}</td>
                         <td>${animal.cantidad}</td>
                     </tr>
                     `;
